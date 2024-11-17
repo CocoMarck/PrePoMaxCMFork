30b4ea285315abd1955bd6934386ffe081eec2e3
MatejB <matej.borovinsek@gmail.com>
2024-11-17 18:51:49 +0100

    DPI line added

--- PrePoMax/Program.cs
+++ PrePoMax/Program.cs
@@ -26,12 +26,20 @@ namespace PrePoMax
         //
         [DllImport("kernel32.dll")]
         static extern bool FreeConsole();
+
+        // ***also dllimport of that function***
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+            // DPI
+            //if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
             //if (IsWindowsApplication()) AttachConsole(ATTACH_PARENT_PROCESS);
             Console.WriteLine("");
             //
